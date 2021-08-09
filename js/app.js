8b9aba6a42039535a545ed5e222ccfbe2776f484
Daniel Ferreira <danthuggin@gmail.com>
2021-08-09 16:23:59 -0300

    Refatorado código conforme feedback passado.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
 const cityForm = document.querySelector('[data-js="change-location"]');
 
-const getWeatherInfos = async event => {
-    const inputValue = event.target.city.value;
-    const [{ Key,  LocalizedName }] = await getCityData(inputValue);
+const getWeatherInfo = async city => {
+    const [{ Key,  LocalizedName }] = await getCityData(city);
     const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key);
-    
-    setDayOrNightBackground(IsDayTime);
-    showOrHideContainer();
-    addIconOnContainer(WeatherIcon);
-    addWeatherInfoOnContainer(LocalizedName, WeatherText, Temperature.Metric);
+    return { IsDayTime, WeatherText, LocalizedName, WeatherIcon, Temperature };
 }
 
-const showOrHideContainer = () => {
-    const cityCardContainer = document.querySelector('[data-js="city-card"]');
-    const isHidden = cityCardContainer.classList.contains('d-none');
-    const showCityCardContainer = isHidden ? 'd-block' : 'd-none';
-
-    cityCardContainer.classList.add(showCityCardContainer);
+const setDayOrNightBackground = IsDayTime => {
+    const bg = document.querySelector('[data-js="bg"]');
+    const timeImg = document.querySelector('[data-js="time"]');
+    const timeImgSrc = IsDayTime ? './src/day.svg' : './src/night.svg';
+    const bgTime = IsDayTime ? 'day-bg' : 'night-bg';
+    
+    bg.classList.remove(...bg.classList)
+    bg.classList.add('bg', bgTime);
+    timeImg.src = timeImgSrc;
 }
 
 const addIconOnContainer = WeatherIcon => {
@@ -27,32 +25,33 @@ const addIconOnContainer = WeatherIcon => {
     timeIconContainer.insertAdjacentHTML('afterbegin', timeIcon);
 }
 
-const setDayOrNightBackground = IsDayTime => {
-    const bg = document.querySelector('.bg');
-    const timeImg = document.querySelector('[data-js="time"]');
-    const timeImgSrc = IsDayTime ? './src/day.svg' : './src/night.svg';
-    const bgTime = IsDayTime ? 'day-bg' : 'night-bg';
+const showOrHideContainer = () => {
+    const cityCardContainer = document.querySelector('[data-js="city-card"]');
+    const isHidden = cityCardContainer.classList.contains('d-none');
     
-    bg.classList.remove(...bg.classList)
-    bg.classList.add('bg', bgTime);
-    timeImg.src = timeImgSrc;
+    if (isHidden) {
+        cityCardContainer.classList.add('d-block');
+    }
+    
 }
 
-const addWeatherInfoOnContainer = (LocalizedName, WeatherText, Metric) => {
+const showCityWeather = async city => {
+    const { IsDayTime, WeatherIcon, LocalizedName, WeatherText, Temperature } = await getWeatherInfo(city)
     const cityNameContainer = document.querySelector('[data-js="city-name"]');
     const cityWeatherContainer = document.querySelector('[data-js="city-weather"]');
     const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]');
     
     cityNameContainer.textContent = LocalizedName;
     cityWeatherContainer.textContent = WeatherText;
-    cityTemperatureContainer.textContent = Metric.Value;
-    
+    cityTemperatureContainer.textContent = Temperature.Metric.Value;
+    setDayOrNightBackground(IsDayTime);
+    addIconOnContainer(WeatherIcon);
+    showOrHideContainer();
 }
-
 
 cityForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    getWeatherInfos(event);
+    showCityWeather(event.target.city.value);
     event.target.reset();
 });
